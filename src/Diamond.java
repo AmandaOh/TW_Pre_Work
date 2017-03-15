f5212db227dbacf1e625e3178a886a283371a5c7
Amanda Oh <[DAmandaGoh>
2017-03-15 17:47:54 +0800

    added diamond with name param

--- src/Diamond.java
+++ src/Diamond.java
@@ -5,16 +5,39 @@ public class Diamond {
 
     private int astCount;
     private int spaceCount;
+    private String name;
 
-    public Diamond(int n) {
-          isosTgl(n);
-          astCount -= 4;
-          spaceCount+= 2;
-          for (int i = 0; i < n -1; i++) {
+    public Diamond(int n, String ...s) {
+          name = s.length > 0 ? s[0] : null;
+
+          if ( name == null ) {
+              isosTgl(n);
+              astCount -= 4;
+              spaceCount+= 2;
+          } else if ( name.length() > 0 ) {
+              spaceCount = n - 1;
+              astCount = 1;
+              for (int i = 0; i < n - 1; i++) {
+                  for (int j = spaceCount; j > 0 ; j--) {
+                      System.out.print(" ");
+                  }
+                  for (int k = 0; k < astCount; k++) {
+                      System.out.print("*");
+                  }
+                  System.out.print("\n");
+                  spaceCount--;
+                  astCount += 2;
+              }
+              System.out.println(name);
+              astCount -= 2;
+              spaceCount++;
+          }
+
+          for (int i = 0; i <= n; i++) {
               for (int j = 0; j < spaceCount; j++) {
                   System.out.print(" ");
               }
-              for (int k = astCount; k >= 0; k--) {
+              for (int k = astCount; k >= 1; k--) {
                   System.out.print("*");
               }
               System.out.print("\n");
@@ -24,13 +47,13 @@ public class Diamond {
     }
 
     public void isosTgl(int n) {
-          astCount = 0;
+          astCount = 1;
           spaceCount = n - 1;
           for (int i = 0; i < n; i ++) {
               for (int j = spaceCount; j > 0; j --) {
                    System.out.print(" ");
               }
-              for (int k = 0; k <= astCount; k++) {
+              for (int k = 0; k < astCount; k++) {
                     System.out.print("*");
               }
               System.out.print("\n");
@@ -38,11 +61,15 @@ public class Diamond {
               spaceCount--;
           }
     }
+
     public static void main(String[] args) {
         System.out.println("Print a Diamond");
-        Diamond d = new Diamond(6);
+        Diamond d = new Diamond(10);
 
         System.out.println("Print a centered triangle");
         d.isosTgl(3);
+
+        System.out.println("Print Diamond with Name");
+        Diamond dName = new Diamond(8, "Amanda");
     }
 }
