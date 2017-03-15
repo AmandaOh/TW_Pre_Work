b6ca6c7afa93ec8c2ddcd1afbae0fe2ce43280fb
Amanda Oh <[DAmandaGoh>
2017-03-15 16:40:43 +0800

    diamond constructor

--- src/Diamond.java
+++ src/Diamond.java
@@ -3,14 +3,34 @@
  */
 public class Diamond {
 
+    private int astCount;
+    private int spaceCount;
+
+    public Diamond(int n) {
+          isosTgl(n);
+          astCount -= 4;
+          spaceCount+= 2;
+          for (int i = 0; i < n -1; i++) {
+              for (int j = 0; j < spaceCount; j++) {
+                  System.out.print(" ");
+              }
+              for (int k = astCount; k >= 0; k--) {
+                  System.out.print("*");
+              }
+              System.out.print("\n");
+              astCount -= 2;
+              spaceCount++;
+          }
+    }
+
     public void isosTgl(int n) {
-          int spaceCount = n - 1;
-          int astCount = 0;
+          astCount = 0;
+          spaceCount = n - 1;
           for (int i = 0; i < n; i ++) {
-              for (int j = spaceCount; j >= 0; j --) {
+              for (int j = spaceCount; j > 0; j --) {
                    System.out.print(" ");
               }
-              for (int j = 0; j <= astCount; j++) {
+              for (int k = 0; k <= astCount; k++) {
                     System.out.print("*");
               }
               System.out.print("\n");
@@ -19,9 +39,10 @@ public class Diamond {
           }
     }
     public static void main(String[] args) {
-        Diamond d = new Diamond();
+        System.out.println("Print a Diamond");
+        Diamond d = new Diamond(6);
+
         System.out.println("Print a centered triangle");
         d.isosTgl(3);
-        
     }
 }
